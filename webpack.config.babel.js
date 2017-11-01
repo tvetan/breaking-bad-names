396a0661b5b3f3a655f4e09cb1d410623725f11b
Tsvetan <tvetan@gbg.bg>
2017-11-01 18:12:47 +0200

    Changed name

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,7 +8,7 @@ export default {
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'starWarsNames',
+    library: 'breakingBadNames',
   },
   devtool: 'source-map',
   module: {
